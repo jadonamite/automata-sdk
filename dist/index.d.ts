32c19cfa1e7e77ef170bb2a89f9723aec3166e63
jadonamite <jadonamite@gmail.com>
2026-05-30 14:59:05 +0100

    clean: remove node_modules and dist from tracking

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { AutomataClient } from './client';
-export type { AutomataClientConfig, ChatParams, ChatResponse, UnsignedTx, AttestParams, AttestResponse, RelayParams, RelayResponse, SaveFlowParams, Flow, SaveTransactionParams, Transaction, WsMessage, } from './types';
